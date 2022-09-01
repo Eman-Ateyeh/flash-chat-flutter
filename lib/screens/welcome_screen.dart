e39a49bbccbab32136af014d4b25deaee1e57c00
Eman <emanatieh2018@gmail.com>
2022-09-01 01:54:58 -0700

    Flash Chat

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,32 +1,57 @@
 import 'package:flutter/material.dart';
+import 'registration_screen.dart';
+import 'login_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
+  static const String id  ="welcome_screen";
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+  AnimationController controller ;
+
   @override
+  void initState() {
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+        upperBound: 100.0,
+        vsync: this, );
+      controller.forward();
+      controller.addListener(()
+      {
+        setState(() {
+
+        });
+        print(controller.value);
+      });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: controller.value,
+                  ),
                 ),
                 Text(
-                  'Flash Chat',
+                  '${controller.value.toInt()}%',
                   style: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
+                    color: Colors.grey,
                   ),
                 ),
               ],
@@ -42,7 +67,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 borderRadius: BorderRadius.circular(30.0),
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to login screen.
+                    Navigator.pushNamed(context, LoginScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
@@ -60,7 +85,8 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 elevation: 5.0,
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to registration screen.
+                    Navigator.pushNamed(context, RegistrationScreen.id);
+                    
                   },
                   minWidth: 200.0,
                   height: 42.0,
